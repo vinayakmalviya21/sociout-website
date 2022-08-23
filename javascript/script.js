d2a1f6a08d881fae435a8b2527dfaed4bfb93b43
hp <vinayakmalviya21@gmail.com>
2022-08-23 13:35:03 +0530

    Login page edited

--- javascript/script.js
+++ javascript/script.js
@@ -1,56 +1,16 @@
-/*-------------- Sidebar ---------------*/
 const menuItems = document.querySelectorAll('.menu-item');
-
-/*-------------- Messages ---------------*/
 
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages')
 const nav = document.querySelector('.container')
 
-
-
-
-/*-------------- Messages ---------------*/
-messagesNotification.addEventListener('click', () => {
-    messages.style.border = 'solid 5px red';
-    setTimeout(() => {
-        messages.style.border = 'none';
-    }, 2000);
-})
-
+/*----------------- Theme Changer ------------------*/
 
 function changeBG(color) {
     document.body.style.backgroundColor = color ;
-    nav.style.backgroundColor = color ;
-
-    
+    nav.style.backgroundColor = color ;  
 }
 
 
 
-
-
-
-
-
-
-
-
-
-// // Background-changes
-// const theme = document.querySelector('#theme');
-// const themeModal = document.querySelector('.customize-theme');
-// var root = documnet.querySelector(':root');
-// const Bg1 = document.querySelector('.bg-1');
-// const Bg2 = document.querySelector('.bg-2');
-
-// // Theme Background values
-// let bgcolor;
-
-// Bg1.addEventListener('click', () => {
-//   // bgcolor = ;
-//     // Bg2.classList.add('active');
-//     // Bg1.classList.remove('active');
-//     // changeBG();
-// })
 
