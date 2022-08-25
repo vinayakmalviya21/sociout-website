3e10fd4065e71d3040d6e0854beff44e89a5342e
Vinayak Malviya <92807452+vinayakmalviya21@users.noreply.github.com>
2022-08-25 19:01:09 +0530

    edited

--- javascript/script.js
+++ javascript/script.js
@@ -3,12 +3,14 @@ const menuItems = document.querySelectorAll('.menu-item');
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages')
 const nav = document.querySelector('.container')
+const post = document.querySelector('#create-post')
 
 /*----------------- Theme Changer ------------------*/
 
 function changeBG(color) {
     document.body.style.backgroundColor = color ;
     nav.style.backgroundColor = color ;  
+    post.style.backgroundColor = color;
 }
 
 
